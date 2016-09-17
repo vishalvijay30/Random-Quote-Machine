7a52279bd97a0df578ac11f39cbcc0758c104d70
Vishal Vijayakumar <sinjaybrothers@gmail.com>
2016-09-17 12:00:37 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,6 @@ $(document).ready(function() {
     $.getJSON("http://quotes.rest/qod.json", function(json) {
       //console.log(json);
       $(".quote-container").html(
-
-
         "<p style=font-size:20px>" + json.contents.quotes[0].quote + "</p>" + "<p style=text-align:right> <br>" + json.contents.quotes[0].author + "</p>"
       );
     });
