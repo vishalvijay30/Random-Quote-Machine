789e70eaa2d4667716aecc61660550dd87f5a97b
Vishal <vishal@lawn-143-215-80-57.lawn.gatech.edu>
2016-09-17 00:07:18 -0400

    All project files

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $(".next-button").on("click", function() {
+    $.getJSON("http://quotes.rest/qod.json", function(json) {
+      //console.log(json);
+      $(".quote-container").html(
+
+
+        "<p style=font-size:20px>" + json.contents.quotes[0].quote + "</p>" + "<p style=text-align:right> <br>" + json.contents.quotes[0].author + "</p>"
+      );
+    });
+  });
+});
